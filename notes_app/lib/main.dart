c9249796cd3f2fb00fa0c70caabe705c054fa91f
beastcode9999 <beastcode9999@gmail.com>
2026-03-06 12:02:10 +0930

    feat: set up boilerplate

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/screens/main_screen.dart';
+
+void main() {
+  runApp(NotesApp());
+}
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      
+    return MaterialApp(
+      title: "Notes App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
     );
   }
 }

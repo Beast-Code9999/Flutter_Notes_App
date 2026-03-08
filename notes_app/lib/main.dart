f02aacda88c72a490bd2e92a5ea5b4ab414b061f
beastcode9999 <beastcode9999@gmail.com>
2026-03-08 23:35:02 +0930

    feat: initialise Hive with noteadapter and note box

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note.dart';
 import 'package:notes_app/screens/main_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  // initialise binding
   WidgetsFlutterBinding.ensureInitialized();
+  // initialise plugin
   await Hive.initFlutter();
-  
+  // Register adapter
+  Hive.registerAdapter(NoteAdapter());
+  // open box
+  await Hive.openBox<Note>('notes');
+  // run app
   runApp(NotesApp());
 }
 

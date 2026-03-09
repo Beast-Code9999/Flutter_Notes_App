ddc284844c753ee55bf68e37557e8bbd78717a90
beastcode9999 <beastcode9999@gmail.com>
2026-03-09 10:58:02 +0930

    feat: add note persistence with Hive local storage

--- notes_app/lib/screens/main_screen.dart
+++ notes_app/lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/models/note.dart';
+import 'package:uuid/uuid.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,7 +14,7 @@ class _MainScreenState extends State<MainScreen> {
   final box = Hive.box<Note>('notes');
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
-  
+  final _uuid = Uuid();
 
   @override
   Widget build(BuildContext context) {
@@ -49,14 +50,28 @@ class _MainScreenState extends State<MainScreen> {
                 actions: [
                   TextButton( // cancel alert dialog and clear controller
                     onPressed: () {
-                      Navigator.pop(context);
                       _titleController.clear();
                       _contentController.clear();
+                      Navigator.pop(context);
                     }, 
                     child: Text("Cancel")
                   ),
                   TextButton( // save note and pop alert dialog
-                    onPressed: () {
+                    onPressed: () { 
+                      // add note if textcontroller is not empty
+                      if(_contentController.text.isNotEmpty && _titleController.text.isNotEmpty) {
+                        setState(() {
+                            box.add(Note( 
+                              id: _uuid.v4(), // use UUID to automatic generation of IDs
+                              title: _titleController.text,
+                              content: _contentController.text, 
+                              createdAt: DateTime.now()
+                          ));
+                        });
+                        _titleController.clear();
+                        _contentController.clear();
+                        Navigator.pop(context);
+                      }
                       // to be added
                     }, 
                     child: Text("Save")
